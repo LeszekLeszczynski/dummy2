329cd9dfaa587add463677a3a6c7213cb6c4cdb0
Leszek Leszczynski <L.Leszczynski@gmail.com>
2019-04-12 21:14:07 +0200

    Test

--- src/main/java/com/ll1138/dummy2/Dummy2Application.java
+++ src/main/java/com/ll1138/dummy2/Dummy2Application.java
@@ -3,11 +3,26 @@ package com.ll1138.dummy2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
 @SpringBootApplication
 public class Dummy2Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Dummy2Application.class, args);
+
+		String p = "";
+
+		for(int i=0; i< 10; i++) {
+			p = p + String.valueOf(i);
+		}
+
+		System.out.println(p);
+
 	}
 
 }
